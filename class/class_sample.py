dc2fe060d9438211c01f58445e253ba40c0f2200
isaccchoi <isaccchoi@naver.com>
2017-09-15 17:09:45 +0900

    class 상속

--- class/class_sample.py
+++ class/class_sample.py
@@ -2,26 +2,36 @@ class Shop(object):
     def __init__(self, name, shop_type, address):
         self.name = name
         self.address = address
-        self.__shop_type = shop_type
+        self._shop_type = shop_type
 
     def show_info(self):
         print("상점정보 (<%s>)"%(self.name))
-        print(f"\t유형: {self.__shop_type}")
+        print(f"\t유형: {self._shop_type}")
         print(f"\t주소: {self.address}")
 
+    @staticmethod
+    def make_dummy():
+        return Shop("untitled", "undefined", "unknown")
+
+
     def change_type(self, new_shop_type):
-        if new_shop_type not in ['패스트푸드', 'PC방']:
-            print('해당 상점 유형은 사용할 수 없습니다.')
-        else:
-            self.__shop_type = new_shop_type
+        self._shop_type = new_shop_type
 
     @property
     def shop_type(self):
         return self.shop_type
 
     @shop_type.setter
-    def name(self, new_name):
-        if new_shop_type not in ['패스트푸드', 'PC방']:
-            print('해당 상점 유형은 사용할 수 없습니다.')
-        else:
-            self.__shop_type = new_shop_type
+    def name(self, new_shop_type):
+        self._shop_type = new_shop_type
+
+
+class Restaurent(Shop):
+    def __init__(self, name, shop_type, address, rating):
+        super().__init__(self, name, shop_type, address)
+        self.raging = rating
+        
+    def show_info(self):
+        print("식당 정보 (<%s>)"%(self.name))
+        print(f"\t유형: {self._shop_type}")
+        print(f"\t주소: {self.address}")
