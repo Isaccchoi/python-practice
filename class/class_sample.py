49878a9e3f4b37659a6c0a9c527bc67eff982f72
isaccchoi <isaccchoi@naver.com>
2017-09-15 15:59:16 +0900

    class공부 attrib, method

---
+++ class/class_sample.py
@@ -0,0 +1,16 @@
+class Shop(object):
+    def __init__(self, name, shop_type, address):
+        self.name = name
+        self.address = address
+        self.shop_type = shop_type
+
+    def show_info(self):
+        print("상점정보 (<%s>)"%(self.name))
+        print(f"\t유형: {self.shop_type}")
+        print(f"\t주소: {self.address}")
+
+    def change_type(self, new_shop_type):
+        if new_shop_type not in ['패스트푸드', 'PC방']:
+            print('해당 상점 유형은 사용할 수 없습니다.')
+        else:
+            self.shop_type = new_shop_type
