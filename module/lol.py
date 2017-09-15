d5a6de2345bd3065ba1824fe135bdc4b04ae613d
isaccchoi <isaccchoi@naver.com>
2017-09-15 15:55:54 +0900

    module 수정

--- module/lol.py
+++ module/lol.py
@@ -1,17 +1,20 @@
 import game
 import shop
+from friends.chat import send_message
 
 def turn_on():
     print('=Turn on game =')
 
     while True:
-        choice = input('What would you like to do?\n 1: Go Go Shop, 2:Play Game, 0: Exit\n Input: ')
+        choice = input('What would you like to do?\n 1: Go Go Shop, 2:Play Game, 3:Message 0: Exit\n Input: ')
         if choice == '0':
             break
         elif choice == '1':
             shop.buy_item()
         elif choice == '2':
             game.play_game()
+        elif choice == '3':
+            send_message()
         else:
             print('Choice not exist')
         print('--------------------------')
